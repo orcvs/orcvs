080e11def1e0f94a951b2df412298098502315cc
Toby Hede <toby@cipherstash.com>
2024-06-23 20:01:39 +1000

    cleanup becnmarks

--- lang/benches/parser_benchmark.rs
+++ lang/benches/parser_benchmark.rs
@@ -19,6 +19,18 @@ fn try_parse(exp: &String) {
     assert!(result.is_ok());
 }
 
+fn interpret(exp: &String) {
+    let mut exp = exp.clone();
+    let mut parser = Parser::new(&mut exp);
+
+    let result = parser.try_parse();
+    assert!(result.is_ok());
+
+    let mut interpreter = Interpreter::new(black_box(&mut parser));
+    let result = interpreter.interpret();
+    assert!(result.is_ok());
+}
+
 pub fn parser_benchmark(c: &mut Criterion) {
     let expressions = [
         String::from("idAA"),
@@ -46,18 +58,6 @@ pub fn parser_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-fn interpret(exp: &String) {
-    let mut exp = exp.clone();
-    let mut parser = Parser::new(&mut exp);
-
-    let result = parser.try_parse();
-    assert!(result.is_ok());
-
-    let mut interpreter = Interpreter::new(black_box(&mut parser));
-    let result = interpreter.interpret();
-    assert!(result.is_ok());
-}
-
 pub fn interpret_benchmark(c: &mut Criterion) {
     let expressions = [
         String::from("idAA"),
@@ -77,30 +77,7 @@ pub fn interpret_benchmark(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, parser_benchmark);
+// criterion_group!(benches, parser_benchmark);
 // criterion_group!(benches, invalid_parser_benchmark);
-// criterion_group!(benches, interpret_benchmark);
-
+criterion_group!(benches, interpret_benchmark);
 criterion_main!(benches);
-
-// pub fn invalid_parser_benchmark(c: &mut Criterion) {
-//     c.bench_function("try_parse_with_invalid", |b| {
-//         b.iter(|| {
-//             let mut s = String::from("id");
-//             let mut parser = Parser::new(black_box(&mut s));
-
-//             let result = parser.try_parse();
-//             let _result = black_box(result);
-//         })
-//     });
-
-//     c.bench_function("parse_with_invalid", |b| {
-//         b.iter(|| {
-//             let mut s = String::from("id");
-//             let mut parser = Parser::new(black_box(&mut s));
-
-//             let result = parser.parse();
-//             let _result = black_box(result);
-//         })
-//     });
-// }
